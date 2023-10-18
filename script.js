b0a19561aa5eb587696eddcad0da6cbdcf00af34
Eduardo Job <venonbrine@gmail.com>
2023-10-18 19:49:34 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function sanda(){
+    window.location.replace("sanda.html")
+}
+
+function sandaini(){
+    window.location.replace("sandaini.html")
+}
+
+function taolu(){
+    window.location.replace("taolu.html") 
+}
